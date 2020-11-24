db4de946f4f1b1f36d1d34b0ba4cb33bc31c2a59
Euclides Bernardo <euclidesquissembebernado@gmail.com>
2020-11-24 19:53:11 +0100

    :zap: Retrofit configuration

--- app/src/main/java/euclidesquissembe/mirepos/data/ReposApi.kt
+++ app/src/main/java/euclidesquissembe/mirepos/data/ReposApi.kt
@@ -1,4 +1,14 @@
 package euclidesquissembe.mirepos.data
 
+import euclidesquissembe.mirepos.util.Constants
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object ReposApi {
+    private fun initRetrofit(): Retrofit = Retrofit.Builder()
+        .baseUrl(Constants.BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    val service: ReposInterface = initRetrofit().create(ReposInterface::class.java)
 }
