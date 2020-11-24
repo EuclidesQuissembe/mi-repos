e07337981e015ecb5a574e44ccd470be62f1eabe
Euclides Bernardo <euclidesquissembebernado@gmail.com>
2020-11-24 19:55:19 +0100

    :sparkles: Function getRepo

--- app/src/main/java/euclidesquissembe/mirepos/data/ReposInterface.kt
+++ app/src/main/java/euclidesquissembe/mirepos/data/ReposInterface.kt
@@ -1,4 +1,13 @@
 package euclidesquissembe.mirepos.data
 
+import euclidesquissembe.mirepos.data.models.Repo
+import retrofit2.http.GET
+import retrofit2.http.Path
+
 interface ReposInterface {
+
+    @GET("{owner}/{repo}")
+    suspend fun getRepo(
+        @Path("owner") owner: String, @Path("repo") repo: String
+    ): Repo
 }
